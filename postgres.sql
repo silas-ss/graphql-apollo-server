8d782a3231df42adfd4e6c4853e93e1d5a4bbe27
Silas Silva <silas.silva@rockt.com.br>
2018-03-22 19:25:26 -0300

    Atualizando script postgres.sql

--- postgres.sql
+++ postgres.sql
@@ -13,21 +13,3 @@ CREATE TABLE public.projects
 WITH (
   OIDS=FALSE
 );
-
--- Table: public.tasks
-
--- DROP TABLE public.tasks;
-
-CREATE TABLE public.tasks
-(
-  id character varying(255) NOT NULL,
-  title character varying(255),
-  description character varying(255),
-  "projectId" character varying(255),
-  "createdAt" timestamp without time zone,
-  "updatedAt" timestamp without time zone,
-  CONSTRAINT tasks_id PRIMARY KEY (id)
-)
-WITH (
-  OIDS=FALSE
-);
